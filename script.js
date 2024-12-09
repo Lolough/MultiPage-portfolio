8a312285776b3c90a2a1b3dcc51b8cb97982b19b
Lolough <Jason.gonzalez547@gmail.com>
2024-12-09 17:43:15 -0500

    deleted it for now
    got to add an input section here

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-//image resizing on same page
 
-document.querySelectorAll('.optionB img').forEach(image => {
-    image.onclick = () =>{
-        document.querySelector('.optionB-popup').style.display = 'block';
-        document.querySelector('.optionB-popup img').src = image.getAttribute('src');
-    }
-});
-
-document.querySelector('.optionB-popup span').onclick = () =>{
-    document.querySelector('.optionB img').style.display = 'none';
-}

1a1570ef722a47073f57df19cafec357f5eb4ffc
Lolough <Jason.gonzalez547@gmail.com>
2024-12-20 13:53:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 //image resizing on same page
-var click = 0;
-function likes(){
-    document.getElementById('counts').innerHTML = ++click;
-    click = click;
+
+let count = 0;
+
+document.querySelectorAll('.optionB img').forEach(graphic => {
+    graphic.onclick = () =>{
+        document.querySelector('.optionB-popup').style.display = 'block';
+        document.querySelector('.optionB-popup img').src = graphic.getAttribute('src');
+    }
+});
+
+        document.querySelector('.optionB-popup span').onclick = () =>{
+        document.querySelector('.optionB-popup').style.display = 'none';
+        }
+        
+let count = 0;
+document.getdocumentbyid('.optionB img').onclick = function(){
+    count += 1;
+    document.getElementById('.counter') = count;
 }
